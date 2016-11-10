3e97138e1bddb1955cd8fa8d26f3947fb7c91d88
Leon Xu <leonxu1999@gmail.com>
2016-11-09 20:09:56 -0800

    password

--- views/settings.js
+++ views/settings.js
@@ -10,8 +10,9 @@ var addClass = function(curClass) {
     //var temp = document.getElementById("currentClasses");
     //temp.innerHTML += "<tr id='remove" + i.id + "'><td>" + i.teacher + "</td><td>" + i.className + "<input type='button' value='remove' class = 'remove' onclick='removeClass(" + i.id + ");'></td></tr>";
     var classJSON = {
-      "classes" : JSON.stringify(ownClasses)
+      "classes" : ownClasses
     };
+    classJSON = JSON.stringify(classJSON);
     console.log(classJSON);
     httpPostAsync(URL + "/add-class", classJSON, function(response) {
       var classes = JSON.parse(response).classes;
